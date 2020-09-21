4278b97dc70a66aeb8668c0a06e2bb096e313969
andrzej <marinemasteras@gmail.com>
2020-09-21 20:53:17 +0200

    redis pubSub and cache

--- src/main/java/com/learning/redis/RedisApplication.java
+++ src/main/java/com/learning/redis/RedisApplication.java
@@ -5,6 +5,8 @@ import com.learning.redis.model.Order;
 import com.learning.redis.model.User;
 import com.learning.redis.repository.LineItemRepository;
 import com.learning.redis.repository.OrderRepository;
+import com.learning.redis.service.OrderService;
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -14,28 +16,40 @@ import lombok.extern.java.Log;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.geo.Circle;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.GeoResults;
 import org.springframework.data.geo.Point;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.Message;
+import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisGeoCommands.DistanceUnit;
 import org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.GeoOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.repository.CrudRepository;
 
+@EnableCaching
 @Log
 @SpringBootApplication
 public class RedisApplication {
 
   @Bean
+  @Profile(value = "old")
   LettuceConnectionFactory connectionFactory() {
     return new LettuceConnectionFactory();
   }
 
   @Bean
+  @Profile(value = "old")
   RedisTemplate<String, User> redisTemplate() {
     RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(connectionFactory());
@@ -89,6 +103,52 @@ public class RedisApplication {
           c -> log.info(c.toString())
       );
     });
+  }
+
+  private final String TOPIC = "chat";
+
+  @Bean
+  ApplicationRunner pubSub (RedisTemplate<String,String> rt){
+    return titledRunner("publish/subscribe", args -> {
+      rt.convertAndSend(TOPIC,"Hello world at " + Instant.now().toString());
+    });
+  }
+
+  @Bean
+  RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory cf){
+    MessageListener ml = new MessageListener() {
+      @Override
+      public void onMessage(Message message, byte[] bytes) {
+        String str = new String(message.getBody());
+        log.info("message from: "+ TOPIC+ " : " +str);
+      }
+    };
+    RedisMessageListenerContainer mlc = new RedisMessageListenerContainer();
+    mlc.setConnectionFactory(cf);
+    mlc.addMessageListener(ml, new PatternTopic(this.TOPIC));
+    return mlc;
+  }
+
+  @Bean
+  CacheManager redisCache (RedisConnectionFactory cf){
+    return RedisCacheManager.builder(cf).build();
+  }
+
+  @Bean
+  ApplicationRunner cache(OrderService orderService) {
+    return titledRunner("caching", args -> {
+      Runnable measure = () -> orderService.orderById(1L);
+      log.info("first: "+ measure(measure));
+      log.info("second: "+ measure(measure));
+      log.info("third: "+ measure(measure));
+    });
+  }
+
+  private long measure (Runnable r){
+    long start = System.currentTimeMillis();
+    r.run();
+    long stop = System.currentTimeMillis();
+    return stop-start;
   }
 
   public static void main(String[] args) {
---
+++ src/main/java/com/learning/redis/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.learning.redis.service;
+
+import com.learning.redis.model.Order;
+import java.util.Collections;
+import java.util.Date;
+import lombok.extern.java.Log;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Log
+@Service
+public class OrderService {
+
+  @Cacheable("order-by-id")
+  public Order orderById (Long id){
+    try{
+      Thread.sleep(1000*10);
+    }
+    catch (Throwable e) {
+      throw new RuntimeException(e);
+    }
+    return new Order(id, new Date(), Collections.emptyList());
+  }
+
+}
